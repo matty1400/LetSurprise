2125d7936e18070067f56f2b00a9ab9d7da21eed
Larsje99 <lars.nolf@telenet.be>
2022-05-05 18:01:41 +0200

    Added CSS + redirect to register page ++ added dropdown menu on every page

--- assets/API/register_user.php
+++ assets/API/register_user.php
@@ -17,18 +17,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>    
+    <title>Document</title>
+    <link href="/assets/css/returning.css" rel="stylesheet" type="text/css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet">    
 </head>
 <body>
-
-<h1 class="Title">
-    USER CREATED
-</h1>
-<a href="/Log_in.html" >
-            Log In
-</a>
-
-    
+    <img src="/assets/images/Logo_LetSurprise2.png" id="logo" alt="Logo">
+    <h1 id="title">
+        USER CREATED
+    </h1>
+    <h3 id="confirmed">
+        Returning to the log-in page
+    </h3>
+    <script>
+        setTimeout("location.href = '../../Log_in.html';", 3000);
+    </script>
 </body>
 </html>
 
