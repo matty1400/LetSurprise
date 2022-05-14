95e042b5dda4091924b35533d810182323f21d7d
hadi <r0893498@student.thomasmore.be>
2022-05-14 15:28:03 +0200

    scam

--- assets/API/insirtEvent.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include "./conn.php";
-    $date = $_GET["date"];
-    $title = $_GET["title"];
-
-
-    $addEvent= procedure("call addEvent($date, $title);");
-    echo json_encode($addEvent);
