af23966d42f9225efe08df27aceb3f7cf5ab294a
Matijs Eraly <matijs.eraly@live.be>
2022-05-16 16:27:13 +0200

    added hash and email check

--- assets/js/Log_in.js
+++ assets/js/Log_in.js
@@ -3,26 +3,35 @@ var loginButtonElement = document.getElementById("Log-in_button");
 
 function getUser(){
     var email = document.getElementById("Email").value;
-    fetch("assets/API/get_credentials.php?email=" + email)
+    var password = document.getElementById("Password").value;
+    console.log(password)
+    fetch("assets/API/get_credentials.php?email=" + email+"&&password="+password)
     .then(function (response){
         return response.json();
     })
     .then(function  (data){
-        var passwordElement = document.getElementById("Password").value;
-        if(passwordElement == data[0].password){
+
+        try{
             sessionStorage.setItem("userID",data[0].personId.toString());
             sessionStorage.setItem("firstname",data[0].firstName);
             sessionStorage.setItem("lastname",data[0].lastName);
-            alert("success");
+            
             
             window.location.href = "http://localhost/indexloggedin.html"
-        }    
+        }
+        catch(err){
+            alert("wrong email or password")
+        };
+        
+       
+        
+            
+            
     });
 };
 
 loginButtonElement.addEventListener("click",function(){
-    var emailElement = document.getElementById("Email").value;
-    var passwordElement = document.getElementById("Password").value;
+    
 
     getUser();
 });
