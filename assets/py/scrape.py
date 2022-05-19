0e671c6d2014aa8c2247da94019d1a6a51b0f579
hadi <r0893498@student.thomasmore.be>
2022-05-19 12:03:34 +0200

    new

---
+++ assets/py/scrape.py
@@ -0,0 +1,30 @@
+from collections import Counter
+from bs4 import BeautifulSoup
+import requests
+import json
+
+wishing = ("djset")
+
+
+requestproduct = requests.get(
+    "https://www.bol.com/be/nl/s/?searchtext=" + wishing).text
+productBS = BeautifulSoup(requestproduct, "html.parser")
+
+# product img
+productimg = productBS.find_all("div", {"class": "h-o-hidden"})
+
+Firstimg = productimg[0]
+
+for link in Firstimg('img'):
+    productimageURL = link.get('src')
+# print(productimageURL)
+
+# product Naam
+productName = productBS.find_all("div", {"class": "product-title--inline"})
+productNameTxt = productName[0].text
+
+# product prijs
+productprijs = productBS.find_all("span", {"class": "promo-price"})
+productpr = productprijs[0].text.strip()
+pijstje = productpr.replace("  -", "")
+# print(pijstje)
