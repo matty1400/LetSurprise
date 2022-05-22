5dd6815d0d44c7fc294836193fa0c1c460cb4654
hadi <r0893498@student.thomasmore.be>
2022-05-22 17:36:01 +0200

    updated

--- assets/python/scrape.py
+++ assets/python/scrape.py
@@ -1,30 +1,119 @@
 from collections import Counter
+from tracemalloc import stop
 from bs4 import BeautifulSoup
 import requests
 import json
+import mysql.connector
+import string
+from nltk.tokenize import word_tokenize
+import numpy as np
 
-wishing = ("djset")
+
+wishing = []
+
+# item krijgen
+try:
+    connection = mysql.connector.connect(host='localhost',
+                                         database='letsurprise',
+                                         user='root',
+                                         password='root')
+
+    sql_select_Query = "select productName from products"
+    cursor = connection.cursor()
+    cursor.execute(sql_select_Query)
+    # get all records
+    records = cursor.fetchall()
+
+    for row in records:
+        wishing.append(row[0])
+
+except mysql.connector.Error as e:
+    print("Error reading data from MySQL table", e)
+finally:
+    if connection.is_connected():
+        connection.close()
+        cursor.close()
 
 
-requestproduct = requests.get(
-    "https://www.bol.com/be/nl/s/?searchtext=" + wishing).text
-productBS = BeautifulSoup(requestproduct, "html.parser")
+itemList = []
+for wish in wishing:
 
-# product img
-productimg = productBS.find_all("div", {"class": "h-o-hidden"})
+    requestproduct = requests.get(
+        "https://www.bol.com/be/nl/s/?searchtext=" + wish).text
+    productBS = BeautifulSoup(requestproduct, "html.parser")
 
-Firstimg = productimg[0]
+    # product img
+    productimg = productBS.find_all("div", {"class": "h-o-hidden"})
 
-for link in Firstimg('img'):
-    productimageURL = link.get('src')
-print(productimageURL)
+    Firstimg = productimg[0]
 
-# product Naam
-productName = productBS.find_all("div", {"class": "product-title--inline"})
-productNameTxt = productName[0].text
+    for link in Firstimg('img'):
+        productimageURL = link.get('src')
+    # print(productimageURL)
 
-# product prijs
-productprijs = productBS.find_all("span", {"class": "promo-price"})
-productpr = productprijs[0].text.strip()
-pijstje = productpr.replace("  -", "")
-print(pijstje)
+    # product Naam
+    productName = productBS.find_all("div", {"class": "product-title--inline"})
+    productNameTxt = productName[0].text
+    productNameTxt2 = productNameTxt.replace("\n", "")
+    # print(productNameTxt)
+
+    # product prijs
+    productprijs = productBS.find_all("span", {"class": "promo-price"})
+    productpr = productprijs[0].text.strip()
+    prijsje = productpr.replace("  -", "")
+    prpr = ""
+    for lala in prijsje:
+        if lala.isdigit() == True:
+            prpr += lala
+        else:
+            prpr += ","
+
+    prpr2 = ""
+
+    for dup in prpr:
+        if dup.isdigit() == True:
+            prpr2 += dup
+
+        if "," not in prpr2 and dup.isdigit() == False:
+            prpr2 += dup
+
+    # print(prpr2)
+
+    xItem = productimageURL, productNameTxt2, prpr2
+
+    # for burger in xItem:
+    itemList.append(xItem)
+
+    # [(productimageURL,productNameTxt,prpr2)
+    # (productimageURL,productNameTxt,prpr2)
+    # (productimageURL,productNameTxt,prpr2)]
+
+    # for x in range(len(wishing)):
+    urlItem = (itemList[len(itemList)-1][0])
+    nameItemx = (itemList[len(itemList)-1][1])
+    priceItem = (itemList[len(itemList)-1][2])
+
+    try:
+        connection = mysql.connector.connect(host='localhost',
+                                             database='letsurprise',
+                                             user='root',
+                                             password='root')
+        insert_stmt = (
+            "INSERT INTO scrapeditems(price, img_url, nameItem, searchName)"
+            "VALUES (%s, %s, %s, %s)"
+        )
+        data = (priceItem, urlItem, nameItemx, wish)
+
+        cursor = connection.cursor()
+        cursor.execute(insert_stmt, data)
+        connection.commit()
+        print(cursor.rowcount, "Record inserted successfully")
+        cursor.close()
+
+    except mysql.connector.Error as error:
+        print("Failed to insert record {}".format(error))
+
+    finally:
+        if connection.is_connected():
+            connection.close()
+            print("MySQL connection is closed")
